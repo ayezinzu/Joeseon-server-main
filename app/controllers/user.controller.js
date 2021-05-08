6b26fcc32814eb24f03dfb91dc65ca1521962e63
Krishna Uppili <krishna.uppili96@gmail.com>
2021-05-08 17:26:00 +0530

    API: get user details API

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,3 +1,7 @@
+const db = require("../models");
+const Sequelize = require("sequelize");
+const User = db.user;
+
 exports.allAccess = (req, res) => {
   res.status(200).send("Public Content.");
 };
@@ -13,3 +17,23 @@ exports.adminBoard = (req, res) => {
 exports.moderatorBoard = (req, res) => {
   res.status(200).send("Moderator Content.");
 };
+
+
+exports.getUser = function(req,res){
+  // Fetch the user by id 
+  User.findOne({_id: req.userId}).then(function(user){
+    var authorities = [];
+    user.getRoles().then(roles => {
+      for (let i = 0; i < roles.length; i++) {
+        authorities.push("ROLE_" + roles[i].name.toUpperCase());
+      }
+      res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roles: authorities,
+          accessToken: req.headers["x-access-token"]
+      });
+    });
+  });
+}
