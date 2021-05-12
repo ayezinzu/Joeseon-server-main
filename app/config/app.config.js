8c514412b827b5475b71f7ab762f5dec797c4e0a
Yogesh Zinzuwadia <ayezinzu@gmail.com>
2021-05-13 05:13:16 +0530

    Configuration: Change Reset URL redirection config

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    host: "http://localhost:3000/auth/reset-password",
+    host: "https://joeseon-dev.herokuapp.com/auth/reset-password",
     SENGRID_KEY: "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"
   };
   
