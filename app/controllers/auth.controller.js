3423a9d27f4dd85af7c7866d3dadfad690b9b13b
Yogesh Zinzuwadia <ayezinzu@gmail.com>
2021-05-13 01:20:20 +0530

    Controllers: Add reset password and image upload APIs

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,15 +1,25 @@
 const db = require("../models");
 const config = require("../config/auth.config");
+const redisConfig = require("../config/redis.config");
+const appConfig  = require("../config/app.config");
 const Sequelize = require("sequelize");
 const User = db.user;
 const Role = db.role;
 const ResetToken = db.token;
+
+var redis = require('redis');
+
+var redisClient = redis.createClient({host : redisConfig.HOST, port : redisConfig.PORT});
 
 const Op = db.Sequelize.Op;
 
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var crypto = require("crypto");
+const nodemailer = require('nodemailer');
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(appConfig.SENGRID_KEY)
 
 exports.signup = (req, res) => {
   // Save User to Database
@@ -70,11 +80,19 @@ exports.signin = (req, res) => {
         expiresIn: 86400 // 24 hours
       });
 
+
       var authorities = [];
       user.getRoles().then(roles => {
         for (let i = 0; i < roles.length; i++) {
           authorities.push(roles[i].name);
         }
+
+        redisClient.hmset(token, {"id" :user.id, "username": user.username, "email": user.email});
+        redisClient.hgetall(token,function(err,reply) {
+            console.log(err);
+            console.log(reply);
+        });
+
         res.status(200).send({
           id: user.id,
           username: user.username,
@@ -87,4 +105,130 @@ exports.signin = (req, res) => {
     .catch(err => {
       res.status(500).send({ message: err.message });
     });
+};
+
+exports.forgotPassword = async (req, res) => {
+//ensure that you have a user with this email
+  console.log("Hello")
+  var email = await User.findOne({where: { email: req.body.email }});
+  console.log(email)
+  if (email == null) {
+    res.json({status: 'ok'});
+  }
+    await ResetToken.update({
+      used: 1
+    },
+    {
+      where: {
+        email: req.body.email
+      }
+   });
+ 
+  //Create a random reset token
+  var token = crypto.randomBytes(64).toString('base64');
+ 
+  //token expires after one hour
+  var expireDate = new Date();
+  expireDate.setDate(expireDate.getDate() + 1/24);
+ 
+  //insert token data into DB
+  await ResetToken.create({
+    email: req.body.email,
+    expiration: expireDate,
+    token: token,
+    used: 0
+  });
+ 
+  //create email
+  const message = {
+      from: 'krishna.uppili96@gmail.com',
+      to: req.body.email,
+      subject: 'Forgot Password',
+      text: 'To reset your password, please click the link below.\n\n'+appConfig.host+'/api/reset_password?token='+token+'&email='+req.body.email
+  };
+ 
+  //send email
+  sgMail
+  .send(message)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+ 
+  res.json({status: 'ok'});
+};
+
+exports.getResetPassword = async (req, res) => {
+   await ResetToken.destroy({
+    where: {
+      expiration: { [Op.lt]: Sequelize.fn('CURDATE')},
+    }
+  });
+ 
+  //find the token
+  var record = await ResetToken.findOne({
+    where: {
+      email: req.query.email,
+      expiration: { [Op.gt]: Sequelize.fn('CURDATE')},
+      token: req.query.token,
+      used: 0
+    }
+  });
+ 
+  if (record == null) {
+    res.json({status: 'error', message: 'Token has expired. Please try again.'});
+  }
+ 
+  res.sendFile(path.join(__dirname + '../../../views/reset_password.html'));
+
+};
+
+
+exports.resetPassword = async (req, res) => {
+  //compare passwords
+  if (req.body.password1 !== req.body.password2) {
+    res.json({status: 'error', message: 'Passwords do not match. Please try again.'});
+  }
+ 
+  var record = await ResetToken.findOne({
+    where: {
+      email: req.body.email,
+      expiration: { [Op.gt]: Sequelize.fn('CURDATE')},
+      token: req.body.token,
+      used: 0
+    }
+  });
+
+  if (record == null) {
+    res.json({status: 'error', message: 'Token not found. Please try the reset password process again.'});
+  }
+  else {
+    var upd = await ResetToken.update({
+      used: 1
+    },
+    {
+      where: {
+        email: req.body.email
+      }
+    });
+
+    var newPassword = bcrypt.hashSync(req.body.password1, 8);
+
+    await User.update({
+      password: newPassword
+    },
+    {
+    where: {
+      email: req.body.email
+    }
+    }).then(result =>
+      res.status(200).json({ message: 'Password reset. Please login with your new password.'})
+    )
+    .catch(err =>
+      res.status(400).json({message: err})
+    );
+  }
+
 };
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,6 +1,31 @@
 const db = require("../models");
 const Sequelize = require("sequelize");
+
+var bcrypt = require("bcryptjs");
+var crypto = require("crypto");
+
+const uuid = require('uuid/v4');
+const mime = require('mime')
+const { Storage }  = require('@google-cloud/storage');
+const Multer = require('multer');
+const {format} = require('util');
+
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
+  },
+});
+
 const User = db.user;
+const Document = db.document
+
+const redisConfig = require("../config/redis.config");
+const gcsConfig = require("../config/gcs.config")
+
+var redis = require('redis');
+
+var redisClient = redis.createClient({host : redisConfig.HOST, port : redisConfig.PORT});
 
 exports.allAccess = (req, res) => {
   res.status(200).send("Public Content.");
@@ -18,11 +43,9 @@ exports.moderatorBoard = (req, res) => {
   res.status(200).send("Moderator Content.");
 };
 
-
 exports.getUser = function(req,res){
   // Fetch the user by id 
-  User.findByPk(req.userId).then(function(user){
-    console.log(req.userId)
+  User.findOne({_id: req.userId}).then(function(user){
     var authorities = [];
     user.getRoles().then(roles => {
       for (let i = 0; i < roles.length; i++) {
@@ -38,3 +61,100 @@ exports.getUser = function(req,res){
     });
   });
 }
+
+exports.uploadImage = async (req, res, next) => {
+  const type = mime.lookup(req.file.originalname);
+  console.log(type)
+	const storage = new Storage({
+		projectId: gcsConfig.google.projectId,
+		keyFilename: "./spherical-bloom-283116-c3d2f8c13036.json",
+	});
+
+	const bucket = storage.bucket(gcsConfig.google.bucket);
+
+  const blob = bucket.file(req.userId+'/'+req.file.originalname);
+  const blobStream = blob.createWriteStream();
+
+  blobStream.on('error', err => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+    // The public URL can be used to directly access the file via HTTP.
+ 
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    );
+    Document.create({
+      status: "pending",
+      name: req.userId+'/'+req.file.originalname
+    })
+    .then(document => {
+      document.setUser(req.userId).then(() => {
+        res.status(200).send({ message: "File uploaded successfully", url: publicUrl, status:document.status });
+      });
+    });
+  });
+  blobStream.end(req.file.buffer);
+}
+
+exports.getImageUrl = async (req,res)=> {
+  const storage = new Storage({
+		projectId: gcsConfig.google.projectId,
+		keyFilename: "./spherical-bloom-283116-c3d2f8c13036.json",
+	});
+
+  const bucket = storage.bucket(gcsConfig.google.bucket);
+
+  Document.findOne({
+    where: {
+      userId: req.userId
+    }
+  })
+  .then(document => {
+    if (!document) {
+      return res.status(404).send({ message: "File not uploaded yet" });
+    }else {
+      file = bucket.file(document.name)
+      return file.getSignedUrl({
+        action: 'read',
+        expires: Date.now() + 15 * 60 * 1000
+      }).then(signedUrls => {
+        console.log(signedUrls[0]);
+        res.status(200).send({ url:  signedUrls[0], status: document.status }); 
+      });
+    }
+  });
+}
+
+exports.changePassword = async (req, res) => {
+  var newPassword = bcrypt.hashSync(req.body.password, 8);
+
+  await User.update({
+    password: newPassword
+  },{
+  where: {
+    id: req.userId
+  }
+  }).then(result =>
+    res.status(200).json({ message: 'Password reset. Please login with your new password.'})
+  )
+  .catch(err =>
+    res.status(400).json({message: err})
+  )
+}
+
+
+exports.logout = function(req,res){
+  let token = req.body.token || req.query.token || req.headers['authorization'] || req.headers['x-access-token'];
+  redisClient.del(token, function(err, response) {
+      if (response == 1) {
+         console.log("Deleted Successfully!")
+         res.status(200).send({"message": "User logged out successfully"});
+      } else{
+       console.log("Cannot delete")
+       res.status(400).send({"success": false, "message": "Token not deleted"});
+      }
+   })
+}
+
